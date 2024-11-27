7c04b332f0f65e03f106a672ef73776424686d0f
nonamebesty <160272703+nonamebesty@users.noreply.github.com>
2024-11-28 00:42:19 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -49,7 +49,7 @@ async def channel_post(client: Client, message: Message):
 
 
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await reply_text.edit(f"<b>{caption} - {file_size}\n\n{link}</b>", reply_markup=reply_markup, disable_web_page_preview = True)
+    await reply_text.edit(f"<b>{caption} ~ [⏰ {duration}] - 📁 {file_size}\n\nLink: {link}</b>", reply_markup=reply_markup, disable_web_page_preview=True)
     
     if not DISABLE_CHANNEL_BUTTON:
         try:
