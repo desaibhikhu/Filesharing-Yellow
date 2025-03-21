0eefa3b4d4e78bb0b3ee2a72f31bdb604a2ab53b
desaibhikhu <85123393+desaibhikhu@users.noreply.github.com>
2025-03-21 11:59:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ APP_ID = int(os.environ.get("APP_ID", "22814216"))
 #your api hash from https://my.telegram.org/apps
 API_HASH = os.environ.get("API_HASH", "62f9bc18f7354d61564aaa7905389081")
 #your channel_id from https://t.me/MissRose_bot by forwarding dummy message to rose and applying command `/id` in reply to that message
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002267482168"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002493134784"))
 #your id of telegram can be found by https://t.me/MissRose_bot with '/id' command
 OWNER_ID = int(os.environ.get("OWNER_ID", "507206621"))
 #port set to default 8080
